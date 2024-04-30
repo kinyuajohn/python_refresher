5ea1539c7625086abc37fcbad496f085be834135
John Kinya <kingjohnkn@gmail.com>
2024-04-30 15:09:48 +0300

    encryption program

--- encryption.py
+++ encryption.py
@@ -9,5 +9,29 @@ chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy()
 
-print(f"chars: {chars}")
-print(f"key: {key}")
+random.shuffle(key)
+
+# print(f"chars: {chars} \n")
+# print(f"key  : {key}")
+
+# Encrypt
+plain_text = input("ENter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"original message: {plain_text}")
+print(f"encrypted message: {cipher_text}")
+
+# Decrypt
+cipher_text = input("ENter a message to encrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message: {plain_text}")
